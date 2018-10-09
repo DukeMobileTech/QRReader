3260997077b67498338635154ffe7d7e88c0ec22
Leonard Ng'eno <dranoel.bravo@gmail.com>
2018-10-09 14:59:54 -0400

    use updated version of pdfbox

--- src/main/java/edu/duke/chpir/QRDecoder.java
+++ src/main/java/edu/duke/chpir/QRDecoder.java
@@ -4,124 +4,59 @@ import com.google.zxing.*;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.rendering.PDFRenderer;
 
-import javax.imageio.ImageIO;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class QRDecoder {
     private static final String DELIMITER = ",";
     private static final String SEPARATOR = "\n";
     private static final String HEADER = "PDF_Name,Page_Number,Decoded_ID";
-    private static final double FACTOR_HALF = 0.5;
-    private static final int FACTOR_DOUBLE = 2;
+    private static final float[] FACTORS = {2.0f, 0.5f, 0.25f};
+    private static int numberOfPages = 0;
+    private static ArrayList<String> csvRows = new ArrayList<>();
 
     private static BinaryBitmap getBinaryBitmap(BufferedImage image) {
         LuminanceSource source = new BufferedImageLuminanceSource(image);
         return new BinaryBitmap(new HybridBinarizer(source));
     }
 
-    private static File createDirectory(String directoryName) {
-        File directory = new File(directoryName);
-        if (!directory.exists()) {
-            directory.mkdir();
-        }
-        return directory;
-    }
-
-    private static BufferedImage getBufferedImage(File file) {
-        BufferedImage image = null;
-        try {
-            image = ImageIO.read(file);
-        } catch (IOException e) {
-            System.err.println("IOException! Cannot read file. " + e.getMessage());
-        }
-        return image;
-    }
-
-    private static String decodeScaled(File qrCodeImage, double factor) {
-        String returnValue = "";
-        BufferedImage before = getBufferedImage(qrCodeImage);
-        int width = before.getWidth(), height = before.getHeight();
-        BufferedImage after = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        AffineTransform affineTransform = new AffineTransform();
-        affineTransform.scale(factor, factor);
-        AffineTransformOp scaleOp = new AffineTransformOp(affineTransform, AffineTransformOp.TYPE_BILINEAR);
-        after = scaleOp.filter(before, after);
-        BinaryBitmap bitmap = getBinaryBitmap(after);
-        try {
-            returnValue = getQRText(bitmap);
-        } catch (NotFoundException e) {
-            System.err.println("NotFoundException: Unable to decode BinaryBitmap. " + e);
-        }
-        return returnValue;
-    }
-
     private static String getQRText(BinaryBitmap bitmap) throws NotFoundException {
-        Map<DecodeHintType, Object>  hints = new EnumMap<>(DecodeHintType.class);
+        Map<DecodeHintType, Object> hints = new EnumMap<>(DecodeHintType.class);
         hints.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
-        BarcodeFormat[] formats = { BarcodeFormat.QR_CODE };
+        BarcodeFormat[] formats = {BarcodeFormat.QR_CODE};
         hints.put(DecodeHintType.POSSIBLE_FORMATS, Arrays.asList(formats));
         Result result = new MultiFormatReader().decode(bitmap, hints);
         return result.getText();
     }
 
-    private static String decodeNormal(File qrCodeImage) {
-        String returnValue = "";
+    private static String decodeImage(BufferedImage qrCodeImage) {
         try {
-            BufferedImage bufferedQRCodeImage = getBufferedImage(qrCodeImage);
-            BinaryBitmap bitmap = getBinaryBitmap(bufferedQRCodeImage);
-            returnValue = getQRText(bitmap);
+            BinaryBitmap bitmap = getBinaryBitmap(qrCodeImage);
+            return getQRText(bitmap);
         } catch (NotFoundException e) {
             System.err.println("NotFoundException: Unable to decode BinaryBitmap. " + e);
+            return "";
         }
-        return returnValue;
     }
 
-    private static String pdfToImage(String pdfFile, String outputFolder) {
-        String value = "";
-        try {
-            createDirectory(outputFolder);
-            File sourceFile = new File(pdfFile);
-            if (sourceFile.exists()) {
-                File destinationSubDir = createDirectory(outputFolder + "/" + sourceFile.getName().replace(".pdf", ""));
-                PDDocument document = PDDocument.load(pdfFile);
-                List<PDPage> pages = document.getDocumentCatalog().getAllPages();
-
-                int pageNumber = 1;
-                for (PDPage page : pages) {
-                    BufferedImage image = page.convertToImage();
-                    File outputFile = new File(destinationSubDir.getAbsolutePath() + "/" + pageNumber + ".png");
-                    ImageIO.write(image, "png", outputFile);
-                    pageNumber++;
-                }
-                document.close();
-                value = destinationSubDir.getAbsolutePath();
-            } else {
-                System.err.println(sourceFile.getName() + " does not exist");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return value;
-    }
-
-    private static void writeCSV(List<String> rows, String outputFolder) {
+    private static void writeCSV(String outputFolder) {
         FileWriter fileWriter = null;
-        String fileName = outputFolder + "/decodedQRCodes.csv";
+        String fileName = outputFolder + "/codes.csv";
         try {
             fileWriter = new FileWriter(fileName);
             fileWriter.append(HEADER);
             fileWriter.append(SEPARATOR);
-            for (String row : rows) {
+            for (String row : csvRows) {
                 fileWriter.append(row);
                 fileWriter.append(SEPARATOR);
             }
@@ -137,7 +72,23 @@ public class QRDecoder {
         }
     }
 
+    private static PDDocument getPDDocument(String pdfFile) {
+        PDDocument pdDocument = null;
+        File sourceFile = new File(pdfFile);
+        if (sourceFile.exists()) {
+            try {
+                pdDocument = PDDocument.load(sourceFile);
+            } catch (IOException e) {
+                System.out.println("Unable to get PDDocument");
+            }
+        } else {
+            System.err.println(sourceFile.getName() + " does not exist");
+        }
+        return pdDocument;
+    }
+
     public static void main(String[] args) {
+        long startTime = System.nanoTime();
         String scansFolder = "";
         String outputFolder = "";
         if (args.length != 2) {
@@ -149,28 +100,43 @@ public class QRDecoder {
         }
 
         File scanSource = new File(scansFolder);
-        ArrayList<String> csvRows = new ArrayList<>();
         for (File file : scanSource.listFiles()) {
             if (file.getName().contains(".pdf")) {
-                String pngDestination = pdfToImage(file.getAbsolutePath(), outputFolder);
-                File imageFolder = new File(pngDestination);
-                for (File imageFile : imageFolder.listFiles()) {
-                    if (imageFile.getName().contains(".png")) {
-                        String row = file.getName().replace(".pdf", "") + DELIMITER
-                                + imageFile.getName().replace(".png", "") + DELIMITER;
-                        String decodedQRCode = decodeNormal(imageFile);
-                        if (decodedQRCode.isEmpty()) {
-                            decodedQRCode = decodeScaled(imageFile, FACTOR_HALF);
-                            if (decodedQRCode.isEmpty()) {
-                                decodedQRCode = decodeScaled(imageFile, FACTOR_DOUBLE);
-                            }
-                        }
-                        System.out.println("Result: " + decodedQRCode);
-                        csvRows.add(row + decodedQRCode);
-                    }
+                PDDocument pdDocument = getPDDocument(file.getAbsolutePath());
+                processPdDocument(file, pdDocument);
+                try {
+                    pdDocument.close();
+                } catch (IOException e) {
+                    System.out.println("Unable to close PDDocument");
                 }
             }
         }
-        writeCSV(csvRows, outputFolder);
+        writeCSV(outputFolder);
+        long endTime = System.nanoTime();
+        long timeTaken = TimeUnit.SECONDS.convert(endTime - startTime, TimeUnit.NANOSECONDS);
+        System.out.println("Number of pages processed: " + numberOfPages);
+        System.out.println("Number of seconds taken: " + timeTaken);
     }
+
+    private static void processPdDocument(File file, PDDocument pdDocument) {
+        PDPageTree pdPageTree = pdDocument.getPages();
+        PDFRenderer pdfRenderer = new PDFRenderer(pdDocument);
+        for (int k = 0; k < pdPageTree.getCount(); k++) {
+            String row = file.getName().replace(".pdf", "") + DELIMITER + (k + 1) + DELIMITER;
+            try {
+                String decodedQRCode = decodeImage(pdfRenderer.renderImage(k));
+                int index = 0;
+                while (decodedQRCode.isEmpty() && index < FACTORS.length) {
+                    decodedQRCode = decodeImage(pdfRenderer.renderImage(k, FACTORS[index]));
+                    index += 1;
+                }
+                numberOfPages += 1;
+                System.out.println("Result: " + decodedQRCode);
+                csvRows.add(row + decodedQRCode);
+            } catch (IOException e) {
+                System.out.println("Unable to convert page to image: " + e);
+            }
+        }
+    }
+
 }
