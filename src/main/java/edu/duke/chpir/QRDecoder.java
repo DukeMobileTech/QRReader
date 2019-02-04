5cfc5215bc904852fde4fac6e2d809cd944fc8e7
Leonard Ng'eno <dranoel.bravo@gmail.com>
2019-02-04 15:46:50 -0500

    re-organize output folder

--- src/main/java/edu/duke/chpir/QRDecoder.java
+++ src/main/java/edu/duke/chpir/QRDecoder.java
@@ -12,19 +12,21 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.EnumMap;
-import java.util.Map;
+import java.nio.file.Files;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class QRDecoder {
     private static final String DELIMITER = ",";
     private static final String SEPARATOR = "\n";
     private static final String HEADER = "PDF_Name,Page_Number,Decoded_ID";
     private static final float[] FACTORS = {2.0f, 0.5f, 0.25f};
+    private static final int DIMENSION = 200;
+    private static final int X = 150;
+    private static final int Y = 50;
     private static int numberOfPages = 0;
-    private static ArrayList<String> csvRows = new ArrayList<>();
 
     private static BinaryBitmap getBinaryBitmap(BufferedImage image) {
         LuminanceSource source = new BufferedImageLuminanceSource(image);
@@ -50,29 +52,6 @@ public class QRDecoder {
         }
     }
 
-    private static void writeCSV(String outputFolder) {
-        FileWriter fileWriter = null;
-        String fileName = outputFolder + "/codes.csv";
-        try {
-            fileWriter = new FileWriter(fileName);
-            fileWriter.append(HEADER);
-            fileWriter.append(SEPARATOR);
-            for (String row : csvRows) {
-                fileWriter.append(row);
-                fileWriter.append(SEPARATOR);
-            }
-        } catch (IOException e) {
-            System.err.println("IOException: " + e.getMessage());
-        } finally {
-            try {
-                fileWriter.flush();
-                fileWriter.close();
-            } catch (IOException e) {
-                System.err.println("IOException: " + e.getMessage());
-            }
-        }
-    }
-
     private static PDDocument getPDDocument(String pdfFile) {
         PDDocument pdDocument = null;
         File sourceFile = new File(pdfFile);
@@ -86,6 +65,18 @@ public class QRDecoder {
             System.err.println(sourceFile.getName() + " does not exist");
         }
         return pdDocument;
+    }
+
+    private static void moveFileToProcessed(String destination, File file) {
+        try {
+            File destinationFolder = new File(destination + "/PROCESSED/");
+            if (!destinationFolder.exists()) {
+                destinationFolder.mkdir();
+            }
+            Files.move(file.toPath(), destinationFolder.toPath().resolve(file.toPath().getFileName()), REPLACE_EXISTING);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
@@ -110,9 +101,9 @@ public class QRDecoder {
                 } catch (IOException e) {
                     System.out.println("Unable to close PDDocument");
                 }
+                moveFileToProcessed(outputFolder, file);
             }
         }
-        writeCSV(outputFolder);
         long endTime = System.nanoTime();
         long timeTaken = TimeUnit.SECONDS.convert(endTime - startTime, TimeUnit.NANOSECONDS);
         System.out.println("Number of pages processed: " + numberOfPages);
@@ -122,8 +113,9 @@ public class QRDecoder {
     private static void processPdDocument(File file, PDDocument pdDocument, String outputFolder) {
         PDPageTree pdPageTree = pdDocument.getPages();
         PDFRenderer pdfRenderer = new PDFRenderer(pdDocument);
+        String baseName = file.getName().replace(".pdf", "");
+        List<String> csvRows = new ArrayList<>();
         for (int k = 0; k < pdPageTree.getCount(); k++) {
-            String baseName = file.getName().replace(".pdf", "");
             String row = baseName + DELIMITER + (k + 1) + DELIMITER;
             try {
                 BufferedImage image = pdfRenderer.renderImage(k);
@@ -133,24 +125,71 @@ public class QRDecoder {
                     decodedQRCode = decodeImage(pdfRenderer.renderImage(k, FACTORS[index]));
                     index += 1;
                 }
+                if (decodedQRCode.isEmpty()) {
+                    int width = DIMENSION, height = DIMENSION, xOrigin = X, yOrigin = Y;
+                    if (image.getWidth() < DIMENSION + X || image.getHeight() < DIMENSION + Y) {
+                        width = image.getWidth();
+                        height = image.getHeight();
+                        xOrigin = 0;
+                        yOrigin = 0;
+                    }
+                    BufferedImage croppedImage = image.getSubimage(xOrigin, yOrigin, width, height);
+                    decodedQRCode = decodeImage(croppedImage);
+                }
                 numberOfPages += 1;
                 System.out.println("Result: " + decodedQRCode);
                 csvRows.add(row + decodedQRCode);
                 if (!decodedQRCode.isEmpty()) {
-                    writeImage(outputFolder + "/" + baseName, image, decodedQRCode);
+                    if (decodedQRCode.length() == 5) {
+                        //Write to CARDS folder
+                        writeImage(outputFolder, "/CARDS/", "", image, decodedQRCode);
+                    } else {
+                        //Write to IDs folder
+                        writeImage(outputFolder, "/IDS/", decodedQRCode.substring(0, 8), image, decodedQRCode);
+                    }
+                } else {
+                    //Write to NO_QR folder
+                    writeImage(outputFolder, "/NOQRS/", baseName, image, (k + 1) + "");
                 }
             } catch (IOException e) {
                 System.out.println("Unable to convert page to image: " + e);
             }
         }
+        writeCSV(outputFolder + "/CSV/", baseName, csvRows);
     }
 
-    private static void writeImage(String outputFolder, BufferedImage image, String imageName) {
+    private static void writeCSV(String outputFolder, String filename, List<String> csvRows) {
+        FileWriter fileWriter = null;
         try {
             File destinationFolder = new File(outputFolder);
+            if (!destinationFolder.exists()) destinationFolder.mkdirs();
+            String fileName = destinationFolder + "/" + filename + ".csv";
+            File file = new File(fileName);
+            file.createNewFile();
+            fileWriter = new FileWriter(fileName);
+            fileWriter.append(HEADER);
+            fileWriter.append(SEPARATOR);
+            for (String row : csvRows) {
+                fileWriter.append(row);
+                fileWriter.append(SEPARATOR);
+            }
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                fileWriter.flush();
+                fileWriter.close();
+            } catch (IOException e) {
+                System.err.println("IOException: " + e.getMessage());
+            }
+        }
+    }
+
+    private static void writeImage(String outputFolder, String subFolder1, String subFolder2, BufferedImage image, String imageName) {
+        try {
+            File destinationFolder = new File(outputFolder + "/" + subFolder1 + "/" + subFolder2);
             if (!destinationFolder.exists()) {
-                destinationFolder.mkdir();
-                System.out.println("Created output folder: " + destinationFolder.getAbsolutePath());
+                destinationFolder.mkdirs();
             }
             File imageFile = new File(destinationFolder.getAbsolutePath() + "/" + imageName + ".png");
             ImageIO.write(image, "png", imageFile);
